15eca1f48404e0e9d455bb8ce43fb7d3fc9bbdec
Siddhesh Zalte <siddheshzalte9370@gmail.com>
2025-09-06 22:52:06 +0530

    third commit

---
+++ JavaAdvanceConcepts/src/patterns/SolidRhombus.java
@@ -0,0 +1,21 @@
+package patterns;
+import java.util.*;
+public class SolidRhombus {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n = sc.nextInt();
+		for (int i = 1;i<=n;i++) {
+			//loop for printing spaces 
+			for (int j = 1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			//for printing stars
+			for(int j = 1;j<=n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
