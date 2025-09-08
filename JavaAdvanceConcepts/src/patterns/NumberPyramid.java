81755cb0c3490e17363aef53539ec7ab250d03ef
Siddhesh Zalte <siddheshzalte9370@gmail.com>
2025-09-08 11:50:28 +0530

    fourth commit

--- JavaAdvanceConcepts/src/patterns/NumberPyramid.java
+++ JavaAdvanceConcepts/src/patterns/NumberPyramid.java
@@ -10,7 +10,7 @@ public class NumberPyramid {
 				System.out.print(" ");
 			}
 			for (int j = 1;j<=i;j++) {
-				System.out.print(a);
+				System.out.print(a+" ");
 			}
 			a++;
 			System.out.println();
