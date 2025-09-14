5751baa54b16e66ea2c01d31d9f91685f13b073a
Siddhesh Zalte <siddheshzalte9370@gmail.com>
2025-09-14 22:29:24 +0530

    twelfth commit

--- JavaAdvanceConcepts/src/oops/FunctionOverloading.java
+++ JavaAdvanceConcepts/src/oops/FunctionOverloading.java
@@ -5,12 +5,16 @@ class Student3{
 	void PrintInfo(String name) {
 		System.out.println("Name:"+name);
 			}
-	void PrintInfo(int age) {
+	void PrintInfo(int age,String name) {
 		System.out.println("Age:"+age);
 			}
 	void PrintInfo(String name,int age) {
 		System.out.println("Name:"+name+" Age:"+age);
 			}
+	 int PrintInfo(int age) {
+		return age+1;
+			}
+	
 	
 }
 public class FunctionOverloading {
@@ -18,7 +22,8 @@ public class FunctionOverloading {
 		Student3 s1 = new Student3();
 		s1.PrintInfo("Siddhesh");
 	    s1.PrintInfo("Siddhesh",22);
-	    s1.PrintInfo(24);
+	    int newage =s1.PrintInfo(24);
+	    System.out.print("NewAge:"+newage);
 		
 	}
 
