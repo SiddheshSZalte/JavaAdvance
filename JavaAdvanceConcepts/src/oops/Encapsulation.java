e78e02c8fee4c7278e49a6ac467b71afe3dc41fb
Siddhesh Zalte <siddheshzalte9370@gmail.com>
2025-09-18 23:50:13 +0530

    Eighteenth Commit

---
+++ JavaAdvanceConcepts/src/oops/Encapsulation.java
@@ -0,0 +1,7 @@
+package oops;
+
+public class Encapsulation {
+	// refers to creating of class and adding attributes and properties in it 
+	// so any code using classes and objects can be consider as example of encapsulation
+
+}
