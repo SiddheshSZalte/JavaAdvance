288e06c81609ad7bd1294b49c1764310354704f0
Siddhesh Zalte <siddheshzalte9370@gmail.com>
2025-09-21 22:43:25 +0530

    slight Changes

--- JavaAdvanceConcepts/src/functions/ExampleFunction.java
+++ JavaAdvanceConcepts/src/functions/ExampleFunction.java
@@ -1,7 +1,7 @@
 package functions;
 import java.util.*;
 public class ExampleFunction {
-	public static  void Name(String a) {
+	static public  void Name(String a) {
 		System.out.println("Name passed from main function is:"+a);
 		return;
 	}
