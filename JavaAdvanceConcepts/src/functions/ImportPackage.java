eaa5ed180775744346a4b2783527d9275a18f53e
Siddhesh Zalte <siddheshzalte9370@gmail.com>
2025-09-22 20:16:32 +0530

    understanding concept of Packages

---
+++ JavaAdvanceConcepts/src/functions/ImportPackage.java
@@ -0,0 +1,9 @@
+package functions;
+import oops.*;// "*" means all
+//import oops.SingleLevelInheritance; to import only 1 file
+public class ImportPackage {
+ static public void main(String args[]) {
+	 SingleLevelInheritance s1 = new SingleLevelInheritance();
+	 // we can create a object of the public class from that package
+ }
+}
