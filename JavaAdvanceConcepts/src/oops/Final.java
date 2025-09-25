113ae0729cebcd8413776c2b80cf26a37dab9fe2
Siddhesh Zalte <siddheshzalte9370@gmail.com>
2025-09-25 23:30:38 +0530

    Final Keyword

--- JavaAdvanceConcepts/src/oops/Final.java
+++ JavaAdvanceConcepts/src/oops/Final.java
@@ -8,12 +8,12 @@ class Name{
 
 public class Final {
 public static void main(String args[]) {
-	final int NUMBER = 2;// FINAL KEYWORD 
-//	NUMBER = 6; CANNOT BE MODIFIED
-	 Name n1 = new Name("Siddhesh");
+	final Integer NUMBER = 2;// FINAL KEYWORD 
+	//NUMBER = 6; //CANNOT BE MODIFIED
+	final Name n1 = new Name("Siddhesh");
 	n1.name = "Aditya";
 	// if a obj is final we can still change its value but cannot reassign it
-	n1 = new Name("new name");
-	System.out.println(n1.name);
+//	n1 = new Name("new name");
+//	System.out.println(n1.name);this cannot be since it is final object
 }
 }
